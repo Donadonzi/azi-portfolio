3a852a6975a0b79e0b8033e258dbdb6612a71cbf
Donadonzi <donadonzi@gmail.com>
2020-04-16 11:48:42 -0400

    responsiveness - footer social menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	$('.portfolio__proj').mouseenter(function(){
-		const id = $(this).attr('id');
-		$(`#${id} .portfolio__proj__description`).slideToggle();
-		$(`#${id} .portfolio__proj__image, #${id} .portfolio__proj__title, #${id} .portfolio__proj__subtitle` ).addClass('hovered');
-	});
+// 	$('.portfolio__proj').mouseenter(function(){
+// 		const id = $(this).attr('id');
+// 		$(`#${id} .portfolio__proj__description`).slideToggle();
+// 		$(`#${id} .portfolio__proj__image, #${id} .portfolio__proj__title, #${id} .portfolio__proj__subtitle` ).addClass('hovered');
+// 	});
 
-	$('.portfolio__proj').mouseleave(function(){
-		const id = $(this).attr('id');
-		$(`#${id} .portfolio__proj__description`).slideUp();
-		$(`#${id} .portfolio__proj__image, #${id} .portfolio__proj__title, #${id} .portfolio__proj__subtitle` ).removeClass('hovered');
-	});
+// 	$('.portfolio__proj').mouseleave(function(){
+// 		const id = $(this).attr('id');
+// 		$(`#${id} .portfolio__proj__description`).slideUp();
+// 		$(`#${id} .portfolio__proj__image, #${id} .portfolio__proj__title, #${id} .portfolio__proj__subtitle` ).removeClass('hovered');
+// 	});
 
 
-});
+// });

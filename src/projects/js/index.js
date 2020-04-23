88c17c3542b199a1869e9f2bd4fc8b5353af5a80
Donadonzi <donadonzi@gmail.com>
2020-04-23 11:00:11 -0400

    fixing responsiveness issues

--- src/projects/js/index.js
+++ src/projects/js/index.js
@@ -12,14 +12,14 @@
 // <!-- to replace @1 and @2 / name: photo.png and photo@2x.png-->
 
 $(document).ready(function () {
-	if (window.devicePixelRatio > 1) {
-		var images = $('img.double-source');
-		images.each(function () {
-			var src1 = $(this).attr('src');
-			var src2 = src1.replace('@1x', '@2x');
-			$(this).attr('src', src2);
-		});
-	}
+	// if (window.devicePixelRatio > 1) {
+	// 	var images = $('img.double-source');
+	// 	images.each(function () {
+	// 		var src1 = $(this).attr('src');
+	// 		var src2 = src1.replace('@1x', '@2x');
+	// 		$(this).attr('src', src2);
+	// 	});
+	// }
 
 	// if ($(window).width() <= 900) {
 	// 	$('#hero-image').attr('src', './img/Meca/Mobile/hero@1x.png');

146d8dd49acc2ce4ab58fb5fdab8c929e9a1aa57
Donadonzi <donadonzi@gmail.com>
2020-04-23 11:40:59 -0400

    fixing bugs - script for changing pix

--- src/projects/js/index.js
+++ src/projects/js/index.js
@@ -12,17 +12,22 @@
 // <!-- to replace @1 and @2 / name: photo.png and photo@2x.png-->
 
 $(document).ready(function () {
-	// if (window.devicePixelRatio > 1) {
-	// 	var images = $('img.double-source');
-	// 	images.each(function () {
-	// 		var src1 = $(this).attr('src');
-	// 		var src2 = src1.replace('@1x', '@2x');
-	// 		$(this).attr('src', src2);
-	// 	});
-	// }
+	if (window.devicePixelRatio > 1) {
+		var images = $('img.double-source');
+		images.each(function () {
+			var src1 = $(this).attr('src');
+			var src2 = src1.replace('@1x', '@2x');
+			$(this).attr('src', src2);
+		});
+	}
 
-	// if ($(window).width() <= 900) {
-	// 	$('#hero-image').attr('src', './img/Meca/Mobile/hero@1x.png');
-	// 	location.reload();
+	// if ($(window).width() <= 768) {
+	// 	const webImages = $('img.double-source');
+	// 	webImages.each(function () {
+	// 		const src1 = $(this).attr('src');
+	// 		const src2 = src1.replace('Web', 'Mobile');
+	// 		$(this).attr('src', src2);
+	// 	});	
 	// }
+	
 });

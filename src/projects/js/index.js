eb64ed32d59098d3d06d3e697c5054a814b85262
Donadonzi <donadonzi@gmail.com>
2020-04-23 11:45:46 -0400

    cleaning up index.js

--- src/projects/js/index.js
+++ src/projects/js/index.js
@@ -1,16 +1,3 @@
-// $(document).ready(function () {
-// 	if (window.devicePixelRatio > 1) {
-// 		var images = $("img.YOUR  IMGS CLASS NAME");
-// 		images.each(function(item) {			
-// 				var src1 = $(this).attr('src');
-// 			  var src2 = src1.replace(/(.*)(\.\w+)/, "$1@2x$2");	
-//         $(this).attr('src', src2);
-// 		});
-// 	}	
-// });
-
-// <!-- to replace @1 and @2 / name: photo.png and photo@2x.png-->
-
 $(document).ready(function () {
 	if (window.devicePixelRatio > 1) {
 		var images = $('img.double-source');

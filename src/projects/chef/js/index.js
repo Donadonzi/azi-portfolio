b7c4156b8d84bb258f03605081f645d939e33590
Donadonzi <donadonzi@gmail.com>
2020-04-29 19:01:11 -0400

    modified index.js

--- src/projects/chef/js/index.js
+++ src/projects/chef/js/index.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
-	if (window.devicePixelRatio > 1) {
-		var images = $('img.double-source');
-		images.each(function () {
-			var src1 = $(this).attr('src');
-			var src2 = src1.replace('@1x', '@2x');
-			$(this).attr('src', src2);
-		});
-	}
+// $(document).ready(function () {
+// 	if (window.devicePixelRatio > 1) {
+// 		var images = $('img.double-source');
+// 		images.each(function () {
+// 			var src1 = $(this).attr('src');
+// 			var src2 = src1.replace('@1x', '@2x');
+// 			$(this).attr('src', src2);
+// 		});
+// 	}
 
-	// if ($(window).width() <= 768) {
-	// 	const webImages = $('img.double-source');
-	// 	webImages.each(function () {
-	// 		const src1 = $(this).attr('src');
-	// 		const src2 = src1.replace('Web', 'Mobile');
-	// 		$(this).attr('src', src2);
-	// 	});	
-	// }
+// 	if ($(window).width() <= 768) {
+// 		const webImages = $('img');
+// 		webImages.each(function () {
+// 			const src1 = $(this).attr('src');
+// 			const src2 = src1.replace('Web', 'Mobile');
+// 			$(this).attr('src', src2);
+// 		});	
+// 	}
 	
-});
+// });
